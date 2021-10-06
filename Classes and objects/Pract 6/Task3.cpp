5ba7d98b05afb05bd861bd080545adedc76a18cc
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2021-10-06 10:40:31 +0530

    Add files via upload

---
+++ Classes and objects/Pract 6/Task3.cpp
@@ -0,0 +1,17 @@
+//Task 3: Reverse a String 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+string s,t,b;
+cout<<"Enter a String\n";
+getline(cin,s);
+int a=s.length();
+string::iterator it;
+for(it=s.end();it>=s.begin();it--)
+{
+t=t+*it;
+}
+cout<<" Reversed String : "<<t<<endl;
+return 0;
+}
