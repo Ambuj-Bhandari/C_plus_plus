9881b4931d9e3edebf3e25ee18e3964c1ab50473
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2022-01-08 22:38:23 +0530

    Update Task2_2.cpp

--- Classes and objects/Practical-16/Task2_2.cpp
+++ Classes and objects/Practical-16/Task2_2.cpp
@@ -16,3 +16,6 @@ int main()
 	} 
 	return 0;
 }
+/* There's no implicit conversion when exceptions are caught,
+   hence 'a' is not converted to int. Therefore, catch(..)
+   which is used to catch all type of exception, gets executed.*/
